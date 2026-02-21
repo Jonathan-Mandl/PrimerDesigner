d93cbee98399e22597d27c05e25918932ad274bf
Jonathan-Mandl <jonathan.mandl2@gmail.com>
2026-02-21 17:54:41 +0200

    Apply .gitignore cleanup

---
+++ test.sh
@@ -0,0 +1,8 @@
+python tool.py --version PD-single-LPath --file_path data/input.txt --output temp
+python tool.py --version PD-mul-ILP --file_path data/input.txt --output temp
+python tool.py --version PD-var-ILP --file_path data/input.txt --output temp
+python tool.py --version PD-mul-Greedy --file_path data/input.txt --output temp
+
+# python -m Comparisons.compare_SpAP
+# python -m Comparisons.compare_CBV3 
+
