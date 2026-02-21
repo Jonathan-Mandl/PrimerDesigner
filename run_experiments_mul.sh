e0d8cf54a60dd242f262a049d6deabede375a113
Jonathan-Mandl <jonathan.mandl2@gmail.com>
2026-02-21 17:59:20 +0200

    Remove shell scripts and ignore them

--- run_experiments_mul.sh
+++
@@ -1,9 +0,0 @@
-# echo "running mul_ILP.py"
-# python -m Experiments.mul_ILP
-
-# echo "running mul_greedy 100 ccds"
-# python -m Experiments.mul_greedy_100_cds
-
-echo "running mul_greedy"
-python -m Experiments.mul_greedy_ILP_comparison
-
--- run_experiments_var.sh
+++
@@ -1,8 +0,0 @@
-echo "running var_ILP_length.py"
-python -m Experiments.var_ILP_length
-
-# echo "running var_ILP_num.py"
-# python -m Experiments.var_ILP_num
-
-# echo "running var_ILP_proteins.py"
-# python -m Experiments.var_ILP_proteins
